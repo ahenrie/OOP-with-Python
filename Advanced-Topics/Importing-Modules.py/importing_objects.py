7c9a3ac476d95f4b6290340437b9b482e2267d1e
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-05-25 15:30:13 -0600

    Create importing_objects.py

---
+++ Advanced-Topics/Importing-Modules.py/importing_objects.py
@@ -0,0 +1,12 @@
+# This file is the program
+import class_definition
+
+emp = class_definition.Employee("Marcia", "VP of Sales")
+emp.display()
+
+########################################################
+from class_definition import Employee, greeting
+
+emp = Employee("Marcia", "VP of Sales")
+emp.display()
+greeting()
