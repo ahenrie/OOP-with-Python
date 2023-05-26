1461f17381fd21d4ca9b38ce837884141beb57b1
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-05-26 13:01:44 -0600

    Create 1.py

---
+++ M6-Coding-Exercises/1.py
@@ -0,0 +1,16 @@
+from tech import Phone, Laptop
+
+my_phone = Phone("Pixel 5", "sage", 128)
+my_laptop = Laptop("MacBook Pro", 15, 256)
+
+print(my_phone)
+print(my_laptop)
+
+############################################
+import tech
+
+my_phone = tech.Phone("Pixel 5", "sage", 128)
+my_laptop = tech.Laptop("MacBook Pro", 15, 256)
+
+print(my_phone)
+print(my_laptop)
